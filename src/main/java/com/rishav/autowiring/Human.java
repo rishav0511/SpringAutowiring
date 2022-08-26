5848f7fa7965b096fe153b755d0386ef59b80926
rishav0511 <rishavraj0511@gmail.com>
2022-08-26 15:03:42 +0530

    Autowire constructor

--- src/main/java/com/rishav/autowiring/Human.java
+++ src/main/java/com/rishav/autowiring/Human.java
@@ -3,6 +3,10 @@ package com.rishav.autowiring;
 public class Human {
     private Heart heart;
 
+    public Human(Heart heart) {
+        this.heart = heart;
+    }
+
     public void setHeart(Heart heart) {
         this.heart = heart;
     }
