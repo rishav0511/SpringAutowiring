a5fb2579030a708e67daa0814c0df6f4346611ab
rishav0511 <rishavraj0511@gmail.com>
2022-08-26 16:25:53 +0530

    Qualifier

--- src/main/java/com/rishav/autowiring/Heart.java
+++ src/main/java/com/rishav/autowiring/Heart.java
@@ -1,6 +1,25 @@
 package com.rishav.autowiring;
 
 public class Heart {
+    private String nameOfAnimal;
+    private int noOfHeart;
+
+    public String getNameOfAnimal() {
+        return nameOfAnimal;
+    }
+
+    public int getNoOfHeart() {
+        return noOfHeart;
+    }
+
+    public void setNameOfAnimal(String nameOfAnimal) {
+        this.nameOfAnimal = nameOfAnimal;
+    }
+
+    public void setNoOfHeart(int noOfHeart) {
+        this.noOfHeart = noOfHeart;
+    }
+
     public void pump() {
         System.out.println("your heart is pumping");
         System.out.println("Alive");
--- src/main/java/com/rishav/autowiring/Human.java
+++ src/main/java/com/rishav/autowiring/Human.java
@@ -1,6 +1,7 @@
 package com.rishav.autowiring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Human {
     private Heart heart;
@@ -9,18 +10,23 @@ public class Human {
 
     }
 
-    @Autowired
     public Human(Heart heart) {
+        System.out.println("Constructor method called");
         this.heart = heart;
     }
 
+    @Autowired
+    @Qualifier("humanHeart")
     public void setHeart(Heart heart) {
+        System.out.println("Setter method called");
         this.heart = heart;
     }
 
     public void startPumping() {
         if(heart!=null) {
             heart.pump();
+            System.out.println("Name of animal: "+heart.getNameOfAnimal());
+            System.out.println("Number of hearts: "+heart.getNoOfHeart());
         } else {
             System.out.println("You are dead.");
         }
